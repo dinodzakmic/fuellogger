add16567fed4d76f18e69b0a1a7b4c3d3eb14d81
Adin <adin.h@codenest.ba>
2016-04-04 10:44:59 +0200

    first commit

---
+++ FuelLogger/FuelLogger/Dashboard.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace FuelLogger
+{
+    public partial class Dashboard : ContentPage
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}

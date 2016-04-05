ef02dfc7679dc12f9365b8105de7e741cd7d206f
Dino Dzakmic <dino.dz@codenest.ba>
2016-04-05 09:22:29 +0200

    SQLite-Net paket dodan i u PCL projekat

---
+++ FuelLogger/FuelLogger/Model/Log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FuelLogger.Model
+{
+    public class Log
+    {
+        
+    }
+}
